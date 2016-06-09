2cc6728a158cdefdc91bc77bcce89a015d6c2556
casa <casa@DESKTOP-GLEF2JS>
2016-06-08 21:32:42 -0300

    nuevo proyecto

--- menu.php
+++ menu.php
@@ -1,12 +1,20 @@
 ﻿<div id='cssmenu'>
 <ul>
+    <head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="css/style.css">
+   <script src="js/jquery-latest.min.js" type="text/javascript"></script>
+   <title>Sweet Dreams</title>
     <img src="images/logo.png" alt=""/>
 <li class='active'><a href='login.php'>iniciar sesion</a></li>
-<li class='has-sub'><a href='catalogo.php'>catalogo</a>
-<li class='has-sub'><a href='contacto.php'>contacto</a>
 <li class='has-sub'><a href='registrar.php'>registrate</a>
+<li class='has-sub'><a href='contacto.php'>contacto</a>
+<li class='has-sub'><a href='catalogo.php'>catalogo</a>
 <li class='has-sub'><a href='home.php'>home</a>
 </li>
+</head>
 
 </ul>
 </div>
