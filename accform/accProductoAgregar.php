8228955e6d37c3ea16709debe248287db87195f3
cetecom <cetecom@LC08L1309>
2016-06-08 21:32:42 -0300

    nuevo proyecto

---
+++ accform/accProductoAgregar.php
@@ -0,0 +1,27 @@
+<?php
+include("../librerias.php");
+session_start();
+
+
+
+
+//var_dump($_POST);
+
+if (!isset($_SESSION["oUsuario"])){
+	?>
+<!-- Reenvio a la página principal-->
+<script>
+	document.location.href="index.php";
+</script>
+<?php
+}
+$valor=$_POST['valor'];
+$snom=$_POST['nombre'];
+$anio=$_POST['anio'];
+
+$oPro=new Producto($nid=NULL,$snom=NULL,$nprec=NULL,$ndia=NULL,$nfecha=NULL);
+
+if($oPro->Ingresar($snom,$valor,$anio)) echo "Producto ingresado"; else echo "ERROR";
+
+
+?>
--- admin.php
+++ admin.php
@@ -1,2 +1,14 @@
 
+<div id='cssmenu'>
+<ul>
+    <?php 
+include('menu.php');
+?>
+    <img src="images/logo.png" alt=""/>
+    <li class='has-sub'><a href='form/agregar.php'>agregar</a>
+    <li class='has-sub'><a href='form/eliminar.php'>eliminar</a>
+    <li class='has-sub'><a href='form/modificar.php'>modificar</a>
+</li>
 
+</ul>
+</div>
